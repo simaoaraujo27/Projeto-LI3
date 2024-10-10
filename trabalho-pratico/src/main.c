e6caff399384931d77fb2854d8130341de34017b
Pedro <a107319@alunos.uminho.pt>
2024-10-10 15:24:57 +0100

    main.c added

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello World!\n");
+int main(int argc, char *argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Missing filename.\n");
+    return 1;
+  }
+
+  char filename = argv[1];
+  FILE *fp = fopen(filename, "r");
+  if (!fp) {
+    perror("Error"); // Imprime "Error: No such file or directory"
+  }
+
   return 0;
 }
