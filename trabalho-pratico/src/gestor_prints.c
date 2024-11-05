789cb883072b0f7e3941e82a625b283c02bb0ee2
Francisco4112 <a107286@uminho.pt>
2024-11-05 15:01:28 +0000

    Update gestor_prints.c

--- trabalho-pratico/src/gestor_prints.c
+++ trabalho-pratico/src/gestor_prints.c
@@ -5,27 +5,26 @@ void print(GList **listaResposta, int numeroArtistas, FILE *newFile) {
   GList *node = *listaResposta;
   char *name;
   char *type;
-  char discografia[10];
+  int discografia;
   char *country;
   char *new_str;
   while (node != NULL) {
     Artists *currentArtist = (Artists *)node->data;
     name = pegarArtistName(currentArtist);
     type = pegarArtistType(currentArtist);
-    snprintf(discografia, sizeof(discografia), "%d",
-             pegarArtistDiscografia(currentArtist));
+    discografia = pegarArtistDiscografia(currentArtist);
     country = pegarArtistCountry(currentArtist);
 
-    int total_len = strlen(name) + strlen(type) + strlen(discografia) +
+    int total_len = strlen(name) + strlen(type) + 4 /*tamanho da discografia*/ +
                     strlen(country) + 4;              // 4 para os ';' e o '\0'
     new_str = malloc((total_len + 1) * sizeof(char)); // +1 para o '\0'
-    snprintf(new_str, total_len + 1, "%s;%s;%s;%s\n", name, type, discografia,
+    snprintf(new_str, total_len + 1, "%s;%s;%d;%s\n", name, type, discografia,
              country);
 
     if (node->next == NULL)
       fprintf(newFile, "%s", new_str);
     else
-      fprintf(newFile, "%s", new_str);
+      fprintf(newFile, "%s\n", new_str);
     node = node->next;
   }
   fclose(newFile);
